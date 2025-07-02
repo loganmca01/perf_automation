f1d69b96cd52c733555471162cfd89a297fb7847
loganmca01 <loganmca01@gmail.com>
2025-07-02 18:06:58 -0400

    started db creation

---
+++ generate_db.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+workload_names = [
+    "TopdownL1",
+    "tma_frontend_bound_group",
+    "tma_backend_bound_group",
+    "tma_fetch_latency_group",
+    "tma_fetch_bandwidth_group",
+    "tma_memory_bound_group",
+    "tma_core_bound_group",
+]
+
+try:
+    with sqlite3.connect(":memory:") as conn:
+        print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
+        
+        cursor = conn.cursor()
+        
+        
+        
+        
+
+except sqlite3.OperationalError as e:
+    print("Failed to open database:", e)
