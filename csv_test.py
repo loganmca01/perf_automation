ca2f57573d3f5d41755ce6ffd0d562d53bdf4ce6
loganmca01 <loganmca01@gmail.com>
2025-07-15 16:59:02 -0400

    bug

--- csv_test.py
+++ csv_test.py
@@ -4,5 +4,6 @@ import run_batch
 times = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
 time_iter = 0
+dev_name = "turing302"
 
 run_batch.extract_data("/home/mcallisl/perf_automation/output_files/ARM-O3-2GiB-128KiB:1024KiB-4-boot-exit-TopdownL1/2025-07-15-14-55-58-turing302", "ARM-O3-2GiB-128KiB:1024KiB-4-boot-exit-TopdownL1-turing302-2025-07-15-14-55-58.csv", times)
